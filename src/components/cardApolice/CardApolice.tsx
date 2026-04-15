ceac6910c8927ef1afc0a0710ae43a0ad599a2ba
BrenoNunes96 <brenocp3@live.com>
2026-04-15 11:02:37 -0300

    card equipe

---
+++ src/components/cardApolice/CardApolice.tsx
@@ -0,0 +1,27 @@
+
+
+export default function CardApolice() {
+  return (
+   <div className="p-4"> 
+      
+ 
+      <div className="bg-white shadow-md border border-gray-200 rounded-lg p-4 w-full max-w-md">
+        
+        <div className="flex items-center gap-4 mb-2">
+    
+          <div className="bg-gray-400 h-12 w-12 rounded-full flex-shrink-0">
+            <img src="" alt="" />
+          </div>
+          
+        
+          <h1 className="text-xl font-bold text-gray-800">Feito para E-Bikes</h1>
+        </div>
+
+        <p className="text-gray-600">
+          Proteção real para motor e bateria, não apenas para o quadro.
+        </p>
+
+      </div>
+    </div>
+  )
+}
--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,6 +1,22 @@
+import Institucional from "../institucional/Institucional"
+
 function Home() {
   return (
-    <div>Home</div>
+    <div>
+    <h1 className="text-[1.35rem] font-bold text-[#103543] mb-8   ml-8   ">
+        Nossa equipe
+      </h1>
+  <Institucional foto="src/assets/image 12.png"dsc="Colaborou na ideia e coordenação do projeto, participando ativamente em todas as etapas, código."  nome="Álvaro César" cargo="SCRUM MASTER"/>
+   <Institucional foto="src/assets/image 8.png" dsc="Colaborou na ideia e coordenação do projeto, participando ativamente em todas as etapas, código." nome="Breno Nunes" cargo="DEV"/>
+    <Institucional foto="src/assets/image 10.png"  dsc="Colaborou na ideia e coordenação do projeto, participando ativamente em todas as etapas, código."   nome="José Rodrigues" cargo="DEV"/>
+     <Institucional foto="src/assets/foto.png"  dsc="Colaborou na ideia e coordenação do projeto, participando ativamente em todas as etapas, código." nome="Maria Eduarda Gomes" cargo="DEV"/>
+      <Institucional foto="src/assets/image 9.png" dsc="Colaborou na ideia e coordenação do projeto, participando ativamente em todas as etapas, código. " nome="Márcia Telles"   cargo="DEV"/>
+       <Institucional foto="src/assets/Foto (1).png"  dsc="Colaborou na ideia e coordenação do projeto, participando ativamente em todas as etapas, código." nome="Mayara Monteiro" cargo="TESTER"/>
+
+
+
+    </div>
+  
   )
 }
 
